2137fd73213d17cc7a12eb4ac61c5f27695eb280
lorenignacio <35474671+lorenignacio@users.noreply.github.com>
2018-04-14 19:07:52 +0800

    Add files via upload

---
+++ ImArray.py
@@ -0,0 +1,11 @@
+#Maria Loren I. Ignacio
+#lorenignacio00@gmail.com
+#This program displays the...
+
+
+arr=[['S','e','c','o','n','d'], ['S','e','m','e','s','t','e','r'], ['i','s'], ['D','o','n','e']]
+arr.append(['P','a','s','s'])
+loren = [arr[0][0],arr[4][1],arr[0][4],arr[4][1]," ",arr[4][0],arr[4][1],arr[1][4],arr[4][1],arr[0][5],arr[3][1]]
+print(loren)
+
+
