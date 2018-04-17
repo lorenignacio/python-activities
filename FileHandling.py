33e08f128ed6017efe6fa9b10ce8d402b2f39f0a
lorenignacio <35474671+lorenignacio@users.noreply.github.com>
2018-04-17 14:57:21 +0800

    Add files via upload

---
+++ FileHandling.py
@@ -0,0 +1,17 @@
+#Maria Loren I. Ignacio
+#https://github.com/lorenignacio
+#lorenignacio00@gmail.com
+
+
+text_file = open("text.txt",'w')
+text_file.write("Welcome! My name is Loren, ")
+text_file.close()
+text_file = open("text.txt",'a')
+text_file.write(" nice meeting you :)")
+text_file.close()
+
+
+# to display in run!
+# text_file = open("text.txt",'r')
+# print(text_file.read())
+# text_file.close()
