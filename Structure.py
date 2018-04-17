4efc83d8775886d6e6b2af8800991f38e5806951
lorenignacio <35474671+lorenignacio@users.noreply.github.com>
2018-04-17 14:47:09 +0800

    Add files via upload

---
+++ Structure.py
@@ -0,0 +1,14 @@
+#Maria Loren I. Ignacio
+#https://github.com/lorenignacio
+#lorenignacio00@gmail.com
+#Remove Duplicate
+
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+from collections import Counter
+letters_counts = Counter(letters)
+repeat = letters_counts.most_common(10)
+print ("letters repeated")
+print(repeat)
