12c0c069b137d8dc7e0f061a60ebb627f2a75521
lorenignacio <35474671+lorenignacio@users.noreply.github.com>
2018-04-17 14:49:32 +0800

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -1,7 +1,6 @@
 #Maria Loren I. Ignacio
 #https://github.com/lorenignacio
 #lorenignacio00@gmail.com
-#Remove Duplicate
 
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
